85b86f2adfa6a755658599fb267d702b558e1735
unknown <kylejohnson7391@gmail.com>
2019-12-05 10:42:17 -0800

    my first commit

---
+++ auto_calibrate/auto_calibrate.ino
@@ -0,0 +1,55 @@
+#include<Servo.h> 
+
+Servo esc;//Declare the ESC as a Servo Object
+Servo elevator;
+Servo rudder;
+
+int x_axis;
+int elevator_val;
+int y_axis;
+int rudder_val;
+int input;
+
+void setup() {
+  esc.attach(10);           //Attach the ESC to Digital Pin 10
+  Serial.begin(9600);      //Begin Reading/Open Serial 
+  Serial.begin (9600) ;
+  pinMode(A1,INPUT);
+  elevator.attach(9);
+  pinMode(A2,INPUT);
+  rudder.attach(8);
+  esc.write(1023);
+  delay(10000);
+  esc.write(508);
+}
+
+void loop() {
+  
+  x_axis();
+  y_axis();
+  
+  input=analogRead(A0);                      //Value of input is analog input on pin A0
+  Serial.print("Original Input Value:");      //Serial print the original input value
+  Serial.print(input);
+  Serial.print(" | ");
+  delay(1);
+  
+  input=map(analogRead(A0), 0, 1023, 0, 180); //Map the input values from the joystick on analog pin 0 to  correspond to max and min values for the servo output: 180 and 0
+  Serial.print("Mapped Input Value:");
+  Serial.print(input);                      //Serial print the mapped input value
+  Serial.print("\n");
+  delay(1);                                  //Delay determines your resolution/smoothness and responsiveness of Motor to the movement of Potwntiometer/Joystick.
+  esc.write(input);
+}
+
+void x_axis(){
+  x_axis=analogRead(A1);
+  elevator_val=map(x_axis,0,1023,0,180);
+  elevator.write(elevator_val);
+}
+
+void y_axis(){
+  y_axis=analogRead(A2);
+  rudder_val=map(y_axis,0,1023,0,180);
+  rudder.write(rudder_val);
+}
